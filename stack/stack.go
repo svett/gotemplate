5d9793996a436d788a254b4d6234bbbf2bbbe96b
Svett Ralchev <svetlin.ralchev@gmail.com>
2015-09-24 13:11:21 +0100

    Add Godoc for stack

--- stack/stack.go
+++ stack/stack.go
@@ -1,3 +1,5 @@
+// This package contains an abstract implementation of Stack
+// data structure
 package stack
 
 import "errors"
@@ -5,14 +7,23 @@ import "errors"
 // template type Stack(TValue)
 type TValue *int
 
+// A structure that represents a stack data structure
+//
+// Example:
+// stack := &stack.Stack{}
+// stack.Push(new(TValue))
+// value, err := stack.Pop()
 type Stack struct {
 	data []TValue
 }
 
+// Adds an element on top of the stack
 func (s *Stack) Push(value TValue) {
 	s.data = append(s.data, value)
 }
 
+// Removes an element from top of the stack.
+// If the stack is empty, it returns an error.
 func (s *Stack) Pop() (TValue, error) {
 	length := len(s.data)
 	if length == 0 {
